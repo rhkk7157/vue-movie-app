c8c7362799d4c8bf3c19add87df4a5b84a6f782b
joy <rhkk7157@gmail.com>
2020-07-10 03:36:15 +0900

    vue-masonry 설치, v-card로 영화목록 출력확인

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>MovieList</div>
+  <v-row v-masonry item-selector=".item">
+      <v-col v-for="movie in movies" :key="movie.imdbID"
+        v-masonry-tile class="item"
+        cols="12" lg="3" md="3" sm="6">
+      <v-card>
+        <v-img
+        :src="movie.Poster"
+        :alt="movie.Title"
+        height="300"></v-img>
+        <v-card-title>{{ movie.Title }}</v-card-title>
+        <v-card-subtitle>{{ movie.Year }}</v-card-subtitle>
+      </v-card>
+      </v-col>
+    </v-row>
 </template>
+
+<script>
+export default {
+  computed: {
+    movies () {
+      return this.$store.state.movie.movies
+    }
+  }
+}
+</script>
