4733165deeaeff3d31130ee3783340904c37881c
joy <rhkk7157@gmail.com>
2020-07-11 03:02:33 +0900

    영화 포스터 검색, 포스터 없는 경우 style

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -9,9 +9,13 @@
         sm="6">
       <v-card>
         <v-img
-          :src="movie.Poster"
+          :src="posterSrc(movie.Poster)"
           :alt="movie.Title"
-          height="300"></v-img>
+          :height="posterHeight(movie.Poster)">
+          <template v-slot:placeholder>
+            <div style="background: lightgray; height: 100%;"></div>
+          </template>
+        </v-img>
         <v-card-title>{{ movie.Title }}</v-card-title>
         <v-card-subtitle>{{ movie.Year }}</v-card-subtitle>
       </v-card>
@@ -24,6 +28,14 @@ export default {
   computed: {
     movies () {
       return this.$store.state.movie.movies
+    }
+  },
+  methods: {
+    posterSrc (poster) {
+      return poster === 'N/A' ? '' : poster
+    },
+    posterHeight (poster) {
+      return poster === 'N/A' ? 100 : 300
     }
   }
 }
--- src/store/movie.js
+++ src/store/movie.js
@@ -13,17 +13,35 @@ export default {
       Object.keys(payload).forEach(key => {
         state[key] = payload[key]
       })
+    },
+    pushIntoMovies (state, movies) {
+      state.movies.push(...movies)
     }
   },
   actions: {
-    async searchMovies ({ state, commit }) {
-      commit('updateState', {
-        loading: true
+    fetchMovies ({ state, commit }, pageNum) {
+      // eslint-disable-next-line no-async-promise-executor
+      return new Promise(async resolve => {
+        const res = await axios.get(`https://www.omdbapi.com/?&apikey=88f9becd&s=${state.title}&page=${pageNum}`)
+        commit('pushIntoMovies', res.data.Search)
+        resolve(res.data)
       })
-      const res = await axios.get(`https://www.omdbapi.com/?&apikey=88f9becd&s=${state.title}`)
-      console.log(res.data)
+    },
+    async searchMovies ({ commit, dispatch }) {
       commit('updateState', {
-        movies: res.data.Search,
+        loading: true,
+        movies: []
+      })
+      const { totalResults } = await dispatch('fetchMovies', 1)
+      const pageLength = Math.ceil(totalResults / 10)
+
+      if (pageLength > 1) {
+        for (let i = 2; i <= pageLength; i++) {
+          if (i > 4) break
+          await dispatch('fetchMovies', i)
+        }
+      }
+      commit('updateState', {
         loading: false
       })
     }
